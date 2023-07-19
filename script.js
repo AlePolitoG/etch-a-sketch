43e10d3b6302dc9b7fe37d1aa7e99e908de367f2
AlePolitoG <polto1010@gmail.com>
2023-07-18 22:59:42 -0500

    add clear functionality
    create function to clear sketch

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function createDiv() {
   for (i = 0; i < squareSize * squareSize; i++) {
     const content = document.createElement("div");
     content.classList.add("content");
+    content.setAttribute("id", "background");
     content.style.width = 320 / squareSize + "px";
     content.style.height = 320 / squareSize + "px";
     container.appendChild(content);
@@ -13,9 +14,23 @@ function createDiv() {
 }
 createDiv();
 
+const colors = document.getElementById("color");
+const size = document.getAnimations("size");
+const rainbow = document.getElementById("rainbow");
+const clear = document.getElementById("clear");
+
 let color = "blue";
 
 container.addEventListener("mouseover", (event) => {
   const element = event.target;
   element.style.backgroundColor = color;
+});
+
+let idAllDiv = document.querySelectorAll("#background");
+let arrIdAllDiv = Array.from(idAllDiv);
+
+clear.addEventListener("click", () => {
+  arrIdAllDiv.forEach((element) => {
+    element.style.backgroundColor = "white";
+  });
 });
